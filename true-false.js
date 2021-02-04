4d2c39c907f4f3064a16b520c5b083515cf41c89
Shofiullah-Developer <shofiullah1762@gmail.com>
2021-02-04 12:10:38 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,13 @@
+//const name="0"
+let name;
+console.log(name);
+if (name) {
+   console.log("condition is true") 
+}
+else{
+    console.log("condition is false")
+}
+// Falsy value 
+// false,0,"",undefined,null,NaN
+// Truthy value 
+// '0',' ',[]
